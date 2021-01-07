fcba24a315bf7b75359ac638abfbadaa08c2a1a3
Zafer CÖMERT <zcomert@samsun.edu.tr>
2021-01-07 14:10:08 +0300

    Business layer has been implemented.

---
+++ CarGalery.Business/Abstract/ICarService.cs
@@ -0,0 +1,17 @@
+﻿using CarGalery.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace CarGalery.Business.Abstract
+{
+    public interface ICarService
+    {
+        List<Car> GetAll(Expression<Func<Car, bool>> filter = null);
+        Car Get(Expression<Func<Car, bool>> filter);
+        void Add(Car entity);
+        void Update(Car entity);
+        void Delete(Car entity);
+    }
+}
---
+++ CarGalery.Business/Concrete/CarManager.cs
@@ -0,0 +1,46 @@
+﻿using CarGalery.Business.Abstract;
+using CarGalery.DataAccess.Abstract;
+using CarGalery.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace CarGalery.Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+
+        // Dependency Injection
+        private ICarDal _car;
+        public CarManager(ICarDal car)
+        {
+            _car = car;
+        }
+
+        public void Add(Car entity)
+        {
+            _car.Add(entity);
+        }
+
+        public void Delete(Car entity)
+        {
+            _car.Delete(entity);
+        }
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            return _car.Get(filter);
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            return _car.GetAll(filter);
+        }
+
+        public void Update(Car entity)
+        {
+            _car.Update(entity);
+        }
+    }
+}
