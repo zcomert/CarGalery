4d699dcd581c3982e392621d392bae798a9dce1a
Zafer CÖMERT <zcomert@samsun.edu.tr>
2021-01-07 13:14:59 +0300

    Entitites....

---
+++ CarGalery.Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarGalery.Entities.Abstract
+{
+    public interface IEntity
+    {
+
+    }
+}
---
+++ CarGalery.Entities/Concrete/Car.cs
@@ -0,0 +1,30 @@
+﻿using CarGalery.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarGalery.Entities.Concrete
+{
+    public class Car : IEntity
+    {
+        // En az üç alan - üç farklı tip
+        public int CarId { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+
+        public Car()
+        {
+
+        }
+        public Car(string model, decimal price)
+        {
+            Model = model;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{CarId,-5} {Model,-30} {Price,-10}";
+        }
+    }
+}
