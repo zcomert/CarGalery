27999a9687c6c0019a57297f8efcd8625e10f971
Zafer CÖMERT <zcomert@samsun.edu.tr>
2021-01-07 14:53:56 +0300

    MVC Project...

---
+++ CarGalery.MVCWebProject/Controllers/CarController.cs
@@ -0,0 +1,20 @@
+﻿using CarGalery.Business.Abstract;
+using CarGalery.Business.CrossCuttingConcerns.DependencyResolvers.Ninject;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarGalery.MVCWebProject.Controllers
+{
+    public class CarController : Controller
+    {
+        public IActionResult Index()
+        {
+            var carService = InstanceFactory.Get<ICarService>();
+            var list = carService.GetAll();
+            return View(list);
+        }
+    }
+}
