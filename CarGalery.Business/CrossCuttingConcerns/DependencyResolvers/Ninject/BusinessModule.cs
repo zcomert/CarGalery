9b03bab827d418bb94275f0c867d11b9dc2505c5
Zafer CÖMERT <zcomert@samsun.edu.tr>
2021-01-07 14:30:18 +0300

    IoC implementation. (Ninject)

---
+++ CarGalery.Business/CrossCuttingConcerns/DependencyResolvers/Ninject/BusinessModule.cs
@@ -0,0 +1,20 @@
+﻿using CarGalery.Business.Abstract;
+using CarGalery.Business.Concrete;
+using CarGalery.DataAccess.Abstract;
+using CarGalery.DataAccess.Concrete.EntityFramework;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarGalery.Business.CrossCuttingConcerns.DependencyResolvers.Ninject
+{
+    public class BusinessModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ICarService>().To<CarManager>().InSingletonScope();
+            Bind<ICarDal>().To<EfCarDal>().InSingletonScope();
+        }
+    }
+}
---
+++ CarGalery.Business/CrossCuttingConcerns/DependencyResolvers/Ninject/InstanceFactory.cs
@@ -0,0 +1,15 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarGalery.Business.CrossCuttingConcerns.DependencyResolvers.Ninject
+{
+    public class InstanceFactory
+    {
+        public static T Get<T>()
+        {
+            return new StandardKernel(new BusinessModule()).Get<T>();
+        }
+    }
+}
